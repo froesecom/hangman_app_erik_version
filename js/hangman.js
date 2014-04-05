609ef22708fb888fcabad5f2121797db8a9f9b00
Erik Froese <froesecom@gmail.com>
2014-04-05 18:30:05 +1100

    integrated Olly's js and mine incorrectGuess function. Modified the inCorrect guess function to work with his code. Moved where the incorrectGuess function is called from, from within the guess fucntion

--- js/hangman.js
+++ js/hangman.js
@@ -1,95 +1,69 @@
-// win === _.Equal(solution, word)
-
-
 $(document).ready(function() {
   $("#gallows").append("<img id='gallows_img' src='img/img8.jpg'>")
   var words = ["word", "anotherword", "fart", "anotherfart"];
 
   var word = ['a','p','p','l','e']   //_.sample(words).split('');
-  var solution = [];
-
-  // var underScore = function () {
-    var wordLength = word.length;
-      var underscores = [];
-      for ( i = 0; i < wordLength; i++) {
-      underscores = underscores + "_ ";
-      // new_solution = underscores.slice();
-      };
-    $("#solution").text(underscores);
-  // };
-
+  var underscores = [];
+  var guessedLetters = [];
   var counter = 8;
 
+  //   var wordLength = word.length;
+  //     for ( i = 0; i < wordLength; i++) {
+  //     underscores = underscores + "_ ";
+  //     // new_solution = underscores.slice();
+  //     };
+  //   $("#solution").text(underscores);
 
+  var guess = function (letter) {
 
-  var guess = function (letter){
-    if (_.contains(word, letter)) { 
-      correctGuess(letter);
-    } else {
-      incorrectGuess(letter);
-    };
+    var solution = [];
+
+    guessedLetters.push(letter);
+
+    _.each(word, function(letter){    
+      if (_.indexOf(guessedLetters, letter) === -1) {
+        solution.push("_"); 
+        $('#solution').text(solution.join(' '));
+      } else {
+        solution.push(letter); 
+        $('#solution').text(solution.join(' '));
+        // if (word. === solution) {
+        //    win();
+        // };
+      };
+    });
+  incorrectGuess(letter);
   };
 
-  var correctGuess = function (letter) {
-    var i = _.indexOf(word, letter);
-    solution[i] = letter;
-    $( "#solution" ).text(solution);
-    console.log(solution[i] = letter);
-    if (_.uniq(word).sort() === solution.sort()) {
-    console.log('win');
-      // win();
-    }; 
-  };
-
-  var incorrectGuess = function (letter) {
-    counter -= 1;
-    $("#guesses").text(counter);
-    $("#gallows").html("<img id='gallows_img' src='img/img" + counter + ".jpg'>")
-    // $('.img' + (counter + 1)).removeClass('img' + (counter + 1)).addClass('img' + counter);
-  //div inner html
-    console.log(counter);
-    if (counter === 0) {
-      $("#guess_inputs").hide();
-      lose();
+ var incorrectGuess = function (letter) {
+    if (_.indexOf(word, letter) === -1) {
+      debugger;
+      counter -= 1;
+      $("#guesses").text(counter);
+      $("#gallows").html("<img id='gallows_img' src='img/img" + counter + ".jpg'>")
+      console.log(counter);
+      if (counter === 0) {
+        $("#guess_inputs").hide();
+        lose();
+      };
     };
   };
   
 
   var lose = function () {
-    console.log('you suck!');   
+    console.log('you suck!');
   };
-  // var win = function () {
-   
+ 
+  
+  var win = function () {
+    console.log('fuck yeah!');
+  };
+
   $("#letter-button").on('click', function(){
     var letter = $('#letter-input').val();
     // debugger;
     guess(letter);
-
   });
+
 });
-
-
-  // var correctGuess = function (letter) {
-     
-      
-  //     word.forEach(function(x) {
-  //       if (x === letter) {
-  //         solution.push(x);
-  //       } else {
-  //         solution.push(" _ ");
-  //       };
-        
-  //     });
-  //     $( "#solution" ).text(solution);
-     
-
-  //     if (_.uniq(word).sort() === solution.sort()) {
-  //     console.log('win');
-  //       // win();
-  //     }; 
-
-  // };
-
-
-
 
