4bb871b83a75b6d2b4f6d83bfc7adfe59db4f456
Erik Froese <froesecom@gmail.com>
2014-04-06 09:25:55 +1000

    A bit of styling done, including fonts. App now choses random word from an array on startup.

--- js/hangman.js
+++ js/hangman.js
@@ -1,68 +1,67 @@
 $(document).ready(function() {
-  $("#gallows").append("<img id='gallows_img' src='img/img8.jpg'>")
-  var words = ["word", "anotherword", "fart", "anotherfart"];
+  // This sets the first image of gallows when the screen is loaded.
+    $("#gallows").append("<img id='gallows_img' src='img/img8.jpg'>")
+    
+  // These are the variables we use throughout the function
+    var words = ["word", "anotherword", "fart", "anotherfart"];
+    var word = _.sample(words).split('');
+    var underscores = [];
+    var guessedLetters = [];
+    var counter = 8;
+  
+    var guess = function (letter) {
 
-  var word = ['a','p','p','l','e']   //_.sample(words).split('');
-  var underscores = [];
-  var guessedLetters = [];
-  var counter = 8;
+      var solution = [];
 
-  //   var wordLength = word.length;
-  //     for ( i = 0; i < wordLength; i++) {
-  //     underscores = underscores + "_ ";
-  //     // new_solution = underscores.slice();
-  //     };
-  //   $("#solution").text(underscores);
+      guessedLetters.push(letter);
 
-  var guess = function (letter) {
+      _.each(word, function(letter){    
+        if (_.indexOf(guessedLetters, letter) === -1) {
+          solution.push("_"); 
+          $('#solution').text(solution.join(' '));
+        } else {
+          solution.push(letter); 
+          $('#solution').text(solution.join(' '));
+          // if (word. === solution) {
+          //    win();
+          // };
+        };
+      });
+    incorrectGuess(letter);
+    };
 
-    var solution = [];
-
-    guessedLetters.push(letter);
-
-    _.each(word, function(letter){    
-      if (_.indexOf(guessedLetters, letter) === -1) {
-        solution.push("_"); 
-        $('#solution').text(solution.join(' '));
-      } else {
-        solution.push(letter); 
-        $('#solution').text(solution.join(' '));
-        // if (word. === solution) {
-        //    win();
-        // };
-      };
-    });
-  incorrectGuess(letter);
-  };
-
- var incorrectGuess = function (letter) {
-    if (_.indexOf(word, letter) === -1) {
-      counter -= 1;
-      $("#guesses").text(counter);
-      $("#gallows").html("<img id='gallows_img' src='img/img" + counter + ".jpg'>")
-      console.log(counter);
-      if (counter === 0) {
-        $("#guess_inputs").hide();
-        lose();
+   var incorrectGuess = function (letter) {
+      if (_.indexOf(word, letter) === -1) {
+        counter -= 1;
+        $("#guesses").text(counter);
+        $("#gallows").html("<img id='gallows_img' src='img/img" + counter + ".jpg'>")
+        console.log(counter);
+        if (counter === 0) {
+          $("#guess_inputs").hide();
+          lose();
+        };
       };
     };
-  };
-  
+    
 
-  var lose = function () {
-    console.log('you suck!');
-  };
- 
-  
-  var win = function () {
-    console.log('fuck yeah!');
-  };
+    var lose = function () {
+      console.log('you suck!');
+    };
+   
+    
+    var win = function () {
+      console.log('fuck yeah!');
+    };
 
-  $("#letter-button").on('click', function(){
-    var letter = $('#letter-input').val();
-    // debugger;
-    guess(letter);
-  });
+    // $("#reset-button").on('click', function() {
+    //   console.log("reset been pressed")
+    //   $( "#gallows" ).empty();
+    //   reset();
+    // });
 
+    $("#letter-button").on('click', function(){
+      var letter = $('#letter-input').val();
+      guess(letter);
+    });
 });
 
